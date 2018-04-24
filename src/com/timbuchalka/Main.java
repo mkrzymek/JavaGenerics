69d81b79f641863cfab42ca53cf9bc0a57531db3
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-24 14:02:56 +0200

    our generics class part 1

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -1,11 +1,11 @@
 package com.timbuchalka;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-
+    private static void genericsIntroduction() {
         ArrayList<Integer> items = new ArrayList<>();
         items.add(1);
         items.add(2);
@@ -22,5 +22,31 @@ public class Main {
         for (int i : n) {
             System.out.println(i * 2);
         }
+    }
+
+
+    private static void ourGenericsClass() {
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+        
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+//        genericsIntroduction();
+        ourGenericsClass();
+
     }
 }
